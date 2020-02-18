84839c18619f789dd0d8bd4d46b2bec4b8b7dc92
zhangxunice <2462832901@qq.com>
2020-02-18 11:28:46 +0800

    授权

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: this.globalData.serverUrl + '/getcode?code=' + res.code,
+          success: (res) => {
+            this.globalData.openId = res.data
+            
+          }
+        })
       }
     })
-    // 获取用户信息
+
+
+
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -20,7 +30,11 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              // console.log(res.userInfo)
+              var userInfo = res.userInfo
+              this.globalData.userInfo = userInfo
+              this.globalData.isLogin=true
+             
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -32,8 +46,41 @@ App({
         }
       }
     })
+
+   
   },
+
+
+  // saveUser: function() {
+  //   console.log(this.globalData.userInfo)
+  //   wx.request({
+  //     url: this.globalData.serverUrl + '/saveuser',
+  //     method: 'POST',
+  //     data: {
+  //       userId: this.globalData.openId,
+  //       nickName: this.globalData.userInfo.nickName,
+  //       faceImage: this.globalData.userInfo.avatarUrl
+  //     },
+  //     success: (res) => {
+  //       console.log(res.data)
+  //     }
+  //   })
+  // },
+
+  // 将用户信息存入缓存
+  setGlobalInfo: function(user) {
+    wx.setStorageSync("userInfo", user)
+  },
+
+  //将用户信息从缓存中取出
+  getGlobalInfo: function() {
+    return wx.getStorageSync("userInfo")
+  },
+
   globalData: {
-    userInfo: null
+    serverUrl: "http://127.0.0.1:8080",
+    openId: '',
+    userInfo: null,
+    isLogin:false
   }
 })
--- pages/me/me.js
+++ pages/me/me.js
@@ -1,66 +1,130 @@
 // pages/me/me.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isLogin: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var isLogin = app.globalData.isLogin
+    var userInfo = app.globalData.userInfo
+    var openId = app.globalData.openId
+
+    this.setData({
+      openId: openId,
+      userInfo:userInfo,
+      isLogin:isLogin
+    })
+
 
   },
+
+
+  getUserInfo: function(e) {
+    this.userAuthorized()
+  },
+
+  //获取用户信息
+  userAuthorized: function() {
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          this.setData({
+            isLogin: true
+          })
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              // console.log(res.userInfo)
+              var userInfo = res.userInfo
+              this.setData({
+                userInfo:userInfo
+              })
+              wx.request({
+                url: app.globalData.serverUrl + '/saveuser',
+                method: 'POST',
+                data: {
+                  userId: this.data.openId,
+                  nickName: userInfo.nickName,
+                  faceImage: userInfo.avatarUrl
+                },
+                success: (res) => {
+                  console.log(res.data)
+                }
+              })
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+
+
+
+
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
